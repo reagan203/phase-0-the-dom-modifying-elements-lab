25c6e130ad03ca04c31bf907a5eb44ffc470ee31
reagan <reaganm746@gmail.com>
2023-09-29 14:26:05 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+function removeMain (deleting){
+    let main =document.querySelectorById('main')
+}
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);
